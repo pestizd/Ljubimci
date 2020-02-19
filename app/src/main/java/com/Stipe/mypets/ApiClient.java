76e502f11e591cfceff2c928d4afa72ba682e4c9
pestizd <44036414+pestizd@users.noreply.github.com>
2020-02-19 01:31:27 +0100

    First

---
+++ app/src/main/java/com/Stipe/mypets/ApiClient.java
@@ -0,0 +1,25 @@
+package com.Stipe.mypets;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+
+class ApiClient {
+
+    private static final String BASE_URL = "http://192.168.43.247/demo_pets/";
+    private static Retrofit retrofit;
+
+    static Retrofit getApiClient(){
+
+        if (retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit;
+    }
+
+}
